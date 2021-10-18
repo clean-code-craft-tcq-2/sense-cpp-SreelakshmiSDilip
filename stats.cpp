5162307679d7a1040410d1b75474515dc826777c
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2021-10-18 20:02:00 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,6 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<double>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<double>& v1 ) {
     //Implement statistics here
+     Stats.max = *max_element(v1.begin(), v1.end());
 }
