032918481670779bcbc0f022ba1774217a976dc3
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2021-10-18 20:05:35 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -2,5 +2,10 @@
 
 Stats Statistics::ComputeStatistics(const std::vector<double>& v1 ) {
     //Implement statistics here
-     Stats.max = *max_element(v1.begin(), v1.end());
+    Stats s;
+     s.max = *max_element(v1.begin(), v1.end());
+    s.min = *min_element(v1.begin(), v1.end());
+    s.average = std::accumulate(v1.begin(), v1.end(), 0LL) / v1.size();
+    
+    return s;
 }
