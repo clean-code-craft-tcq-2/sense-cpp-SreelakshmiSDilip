46954a01b4255e8cd1fdc9f14848a4f75d7156c6
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2021-10-18 19:48:15 +0530

    change_2

--- stats.h
+++ stats.h
@@ -7,5 +7,5 @@ namespace Statistics {
         float minVal;
         float maxVal;
     };
-    Stats ComputeStatistics(const std::vector<___>& );
+    Stats ComputeStatistics(const std::vector<int>& );
 }
