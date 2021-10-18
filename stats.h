4183efbde64279a88df0ab9fe2b28316f0dee770
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2021-10-18 20:54:40 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -11,9 +11,18 @@ namespace Statistics {
 }
 class StatsAlerter
 {
-   std::vector<IAlerter*> alerters;
-   float maxThreshold = 10.2;
+	using IAlerter = boost::variant<EmailAlert, LEDAlert>;
+    std::vector<IAlerter*> alerters;
+    float maxThreshold = 10.2;
 public:
    void checkAndAlert( std::vector<IAlerter*>);
-}
+};
+struct EmailAlert
+{
+	bool emailSent;
+};
+struct LEDAlert
+{
+	bool ledGlows;
+};
 
