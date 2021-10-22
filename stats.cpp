627de27f8755c7f571337c588cbe4541423b4d60
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2021-10-22 19:26:45 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -13,7 +13,7 @@ Stats ComputeStatistics(const std::vector<double>& v1 ) {
 }
     
 }
-void checkAndAlert( const std::vector<float>& v2)
+void checkAndAlert( const std::vector<double>& v2)
 {
     auto computedStats = Statistics::ComputeStatistics(v2);
     if(computedStats.max > statsAlerter.maxThreshold)
