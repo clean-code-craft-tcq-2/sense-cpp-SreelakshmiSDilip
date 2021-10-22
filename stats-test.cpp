8c9dbdaaf9765d41df56dd4ebd6fb9eb9630a204
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2021-10-22 21:41:07 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -29,7 +29,7 @@ TEST_CASE("raises alerts when max is greater than threshold") {
 }
 
 TEST_CASE("average is NaN for empty array") {
-    auto computedStats = Statistics::ComputeStatistics({});
+    auto computedStats = Statistics::ComputeStatistics({NAN,NAN,NAN});
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
     REQUIRE(std::isnan(computedStats.average));
