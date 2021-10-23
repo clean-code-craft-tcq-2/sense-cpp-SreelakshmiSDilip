94daef7378850d025a7044fd757478545233c25c
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2021-10-23 21:05:09 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -3,27 +3,27 @@
 #include<numeric>
 
 namespace Statistics {
-Stats ComputeStatistics(const std::vector<double>& v1 ) {
-    //Implement statistics here
-    Stats s;
-     s.max = *(std::max_element(v1.begin(), v1.end()));
-    s.min = *(std::min_element(v1.begin(), v1.end()));
-    s.average = (std::accumulate(v1.begin(), v1.end(),0.0L)) / v1.size();
-    
-    return s;
-}
-    
+    Stats ComputeStatistics(const std::vector<double>& v1 ) {
+        //Implement statistics here
+        Stats s;
+        s.max = *(std::max_element(v1.begin(), v1.end()));
+        s.min = *(std::min_element(v1.begin(), v1.end()));
+        s.average = (std::accumulate(v1.begin(), v1.end(),0.0L)) / v1.size();
+
+        return s;
+    }
+
 }
 void StatsAlerter::checkAndAlert( const std::vector<double>& v2)
 {
     auto computedStats = Statistics::ComputeStatistics(v2);
     if(computedStats.max > maximumThreshold)
     {
-        *(vecAlert[0]->emailAlert1) = {true};
-        *(vecAlert[0]->ledAlert1) = {true};
+        *(vecAlert[0]->emailAlertPtr) = {true};
+        *(vecAlert[0]->emailAlertPtr) = {true};
     }
-    
-    
 
 }
+
+
 
