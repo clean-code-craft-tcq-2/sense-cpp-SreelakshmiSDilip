125bf0cd7bd15a1d13c765cd0e693a8926896ce8
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2021-10-23 21:03:57 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -8,39 +8,40 @@ namespace Statistics {
         double max;
     };
     Stats ComputeStatistics(const std::vector<double>& );
-	
-}
-//namespace
-//{
-	struct EmailAlert 
-	{
-		bool emailSent;
-	};
-	struct LEDAlert 
-	{
-		bool ledGlows;
-	};
-	struct IAlerter
-	{
-		EmailAlert *emailAlert1;
-		 LEDAlert *ledAlert1;
-	};
-	
-	class StatsAlerter
-	{
-          float maximumThreshold;
-	  std::vector<IAlerter*>vecAlert;
-	  
-	public:
-	   StatsAlerter(const float Threshold,  std::vector<IAlerter*> vecAlertArg )
-	   {
-		  maximumThreshold =  Threshold;
-		   vecAlert = vecAlertArg;
-	   }
-	   //std::vector<IAlerter> checkAndAlert( const std::vector<double>&);
-	    void checkAndAlert( const std::vector<double>&);
-	};
 
-//}
+}
+
+struct EmailAlert 
+{
+    bool emailSent;
+};
+struct LEDAlert 
+{
+    bool ledGlows;
+};
+struct IAlerter
+{
+    EmailAlert *emailAlertPtr;
+    LEDAlert *ledAlertPtr;
+};
+
+class StatsAlerter
+{
+    float maximumThreshold;
+    std::vector<IAlerter*>vecAlert;
+
+public:
+    StatsAlerter(const float Threshold,  std::vector<IAlerter*> vecAlertInit )
+    {
+        maximumThreshold =  Threshold;
+        vecAlert = vecAlertInit;
+    }
+    //std::vector<IAlerter> checkAndAlert( const std::vector<double>&);
+    void checkAndAlert( const std::vector<double>&);
+};
+
+
+
+
 
 
