7aef6098b3d5f8e81d85370e1ec23a9ff15afd23
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2021-10-23 19:51:25 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -14,20 +14,16 @@ Stats ComputeStatistics(const std::vector<double>& v1 ) {
 }
     
 }
-IAlerter StatsAlerter::checkAndAlert( const std::vector<double>& v2)
+void StatsAlerter::checkAndAlert( const std::vector<double>& v2)
 {
     auto computedStats = Statistics::ComputeStatistics(v2);
     IAlerter localStruct;
     if(computedStats.max > maximumThreshold)
     {
-        localStruct.emailAlert1.emailSent = true;
-        localStruct.ledAlert1.ledGlows = true;
-//vecAlert.push_back(localStruct);
-        vecAlert[0]->emailAlert1 = localStruct.emailAlert1;
-        vecAlert[0]->ledAlert1= localStruct.ledAlert1;
+       
     }
     
-    return (*vecAlert[0]);
+    
 
 }
 
